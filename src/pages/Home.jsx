f80539c9eb868420f32453bb64756a7342a95c23
Ruffy-Deve <rufaihassan2000@gmail.com>
2026-03-23 06:25:24 +0100

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import StatsCounter from "../components/StatsCounter";
 import StaffSection from "../components/StaffSection";
 import JobCard from "../components/JobCard";
-import { JOBS } from "../data/jobs";
+import { JOBS } from "../data/Jobs";
 
 gsap.registerPlugin(ScrollTrigger);
 
